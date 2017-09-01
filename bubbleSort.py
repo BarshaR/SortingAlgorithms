b304346628691593b4ee1690e26f36e861c3d029
Richard Barsha <Richard@Richards-iMac.local>
2017-09-01 23:23:08 +1000

    Users can now input either a string or file to sort through

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,21 +1,10 @@
-# User specifies files or text as the input
-# If text, input a string separated by comma's
-# If file, pass in absolute path to the file
-
-inputType = input("Choose input type: \n 1. Text \n 2. File")
-print("You selected: ", inputType)
-
-numbers = [8,2,3,5,4,12,7,6,20,15,67,34,54,64]
-
-
-#numbers = [1,2,3,6,5,7,8,9]
-
+# Swap two items in a list
 def swap (pos1, pos2):
     temp = numbers[pos1]
     numbers[pos1] = numbers[pos2]
     numbers[pos2] = temp
 
-
+# Main sorting Logic
 def bubbleSort (numbers):
     swapNum = 0
     comparisonNum = 0
@@ -28,11 +17,48 @@ def bubbleSort (numbers):
                 didSwap = True
                 swapNum = swapNum + 1
         if not didSwap:
-            print("Early terminate Swaps: ", swapNum)
-            print("Comparisons: ",comparisonNum)
+            print('Early terminate Swaps: ', swapNum)
+            print('Comparisons: ',comparisonNum)
             return
-    print("Swaps: ", swapNum)
-    print("Comparisons: ",comparisonNum)
+    print('Swaps: ', swapNum)
+    print('Comparisons: ',comparisonNum)
+
+# Convert a comma separated string to a list of inte
+def stringToList (string):
+    list = string.split(',')
+    newList = []
+    for item in list:
+        newList.append(int(item))
+    # Return list of integers
+    return newList
+
+# Process File input
+def processFile (pathname):
+    # Open file and read data removing '\n'
+    # filename = '/Users/Richard/Desktop/numberList.txt'
+    file = open(pathname, 'r')
+    fileContents = file.read().split('\n')
+    list = fileContents[0]
+    # return a list of integers
+    return stringToList(list)
+
+# User specifies files or text as the input
+# If text, input a string separated by comma's
+# If file, input absolute path to the file
+
+inputType = input('Choose input type \n 1. Text \n 2. File\n\n Input: ')
+# Validate user input
+if (inputType == '1' or inputType == 'Text' or inputType == 'text'):
+    print('You selected Text')
+    enteredList = input('Enter comma separated integers\n\nList: ')
+    numbers = stringToList(enteredList)
+elif (inputType == '2' or inputType == 'File' or inputType == 'file'):
+    print('You selected File')
+    pathname = input('Enter file path\npath: ')
+    numbers = processFile(pathname)
+else:
+    print('Invalid Input, quitting now')
+    quit()
 
 bubbleSort(numbers)
 print(numbers)
